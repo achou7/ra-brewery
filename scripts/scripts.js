e71b1208fa7c96fae30e7c4a81019fba2606648a
Alvin <achou7@gmail.com>
2019-02-21 22:28:50 -0800

    -autoscroll dot fix, added in mobile scripts
    -changed scripts to not crash on other pages
    -added in hamburger click handler

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,15 @@
 // var slideIndex = 1;
 // showSlides(slideIndex);
 
-// // Thumbnail image controls
+// Thumbnail image controls
+
 // function currentSlide(n) {
 //     showSlides(slideIndex = n);
 //   }
   
 //   function showSlides(n) {
 //     var i;
+    
 //     var slides = document.getElementsByClassName("my-slides");
 //     var dots = document.getElementsByClassName("dot");
 //     if (n > slides.length) {slideIndex = 1} 
@@ -17,7 +19,8 @@
 //     for (i = 0; i < slides.length; i++) {
 //         slides[i].style.display = "none"; 
 //     }
-//     for (i = 0; i < dots.length; i++) {
+//     for (j = 0; j < dots.length; j++) {
+       
 //         dots[i].className = dots[i].className.replace(" active", "");
 //     }
 //     slides[slideIndex-1].style.display = "block"; 
@@ -25,20 +28,32 @@
 //   }
 
 //autoslide
-
-var slideIndex = 0;
-showSlides();
+var slides = document.getElementsByClassName("my-slides");
+if(slides.length != 0) {
+  var slideIndex = 0;
+  showSlides();
+}
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("my-slides");
-  
+  var dots = document.getElementsByClassName("dot"); 
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
+    dots[i].classList.remove("active");
+
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].classList.add("active");
   setTimeout(showSlides, 4000); //change every 4 secs
 }
 
+
+
+
+$('.hamburger').click(function() {
+  $('.hamburger span').toggleClass('selected');
+  $('nav').toggleClass('open');
+});
