6963bc64fcc4f5dccaaba9c4f3b28523d924e4a4
Alvin Chou <achou31@my.bcit.ca>
2019-02-27 11:58:58 -0800

    -finished products and beers page -added in files and variables -added in styles for nav and scrolling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,44 @@ $('.hamburger').click(function() {
   $('nav').toggleClass('open');
   $('nav').toggleClass('close');
 });
+
+$('.scroll-link').click(function() {
+  
+  let el = $(this).attr('href');
+  console.log($(el).offset().top);
+  $('html, body').animate(
+    {scrollTop: $(el).offset().top - 30}
+  , 1000)
+});
+
+$('.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1000);
+});
+
+$(window).scroll(function() {
+  console.log(parseInt($(window).height()*2));
+  console.log($('html,body').scrollTop());
+  if($('html, body').scrollTop() < parseInt($(window).height()/2)) {
+    $('.back-to-top').css('opacity', '0');
+  }
+  else {
+    $('.back-to-top').css('opacity', '1');
+  }
+  if($('.beer-content').length > 0) {
+    $('.beer-content').get().forEach(function(el) {
+      console.log($('html, body').scrollTop());
+      if($('html, body').scrollTop() >= ($(el).offset().top - 200)) {
+          $(el).addClass('fadeIn');
+          $(el).removeClass('fadeOut');    
+      }
+      else {
+        $(el).addClass('fadeOut');
+        $(el).removeClass('fadeIn');
+      }
+
+   
+    });
+  }
+});
